db5df47e3802948388504cbfceef82508fd41a18
Buster Hultgren Wärn <buster.hultgrenwarn@tietoevry.com>
2023-10-01 16:13:18 +0200

    Implemented logic and tests

--- caesar/lib/caesar/cipher.ex
+++ caesar/lib/caesar/cipher.ex
@@ -4,15 +4,44 @@ defmodule Caesar.Cipher do
   """
 
   @doc """
-  Hello world.
+  [Caesar Cipher](https://en.wikipedia.org/wiki/Caesar_cipher).
 
   ## Examples
 
-      iex> Caesar.Cipher.hello()
-      :world
+      iex> Caesar.Cipher.encrypt("Hello World!", -10)
+      "Rovvy Gybvn!"
 
   """
-  def hello do
-    :world
+
+def encrypt(string, shift) when is_binary(string) and is_integer(shift) do
+  # Convert msg to char_list
+  string
+    |> to_charlist()
+  # Iterate over list apply shift mapping
+    |> Enum.map(&shift_char(&1, shift))
+  # Return list as binary string
+    |> List.to_string
   end
+
+  defp shift_char(char, shift) do
+    case char do
+      chr when chr in (?a..?z) -> calculate_mapping(?a, chr, shift)
+      chr when chr in (?A..?Z) -> calculate_mapping(?A, chr, shift)
+      chr -> chr
+    end
+  end
+
+  @alphabet_size 26
+
+  defp calculate_mapping(base_letter, char, shift) do
+    # Find the ASCII integer of the char and normalize it
+    # by subtracting the size of the alphabet.
+    normalize = &(&1 - @alphabet_size)
+    # Ensure shift number is within the alphabet size
+    shift_num = rem(shift, @alphabet_size)
+    # Calculate the shifted value
+    base_letter + rem(char - normalize.(base_letter) - shift_num, @alphabet_size)
+
+  end
+
 end
--- caesar/test/caesar/cipher_test.exs
+++ caesar/test/caesar/cipher_test.exs
@@ -2,7 +2,30 @@ defmodule Caesar.CipherTest do
   use ExUnit.Case
   doctest Caesar.Cipher
 
-  test "greets the world" do
-    assert Caesar.Cipher.hello() == :world
+  import Caesar.Cipher
+
+  test "Simple encrypt shift right" do
+    assert encrypt("abcd", 1) === "zabc"
   end
+
+  test "Simple encrypt shift left" do
+    assert encrypt("abcd", -1) === "bcde"
+  end
+
+  test "Encrypt handles capital letters" do
+    assert encrypt("AbCd", 1) === "ZaBc"
+  end
+
+  test "Encrypt messages with spaces" do
+    assert encrypt("Hello World!", -10) === "Rovvy Gybvn!"
+  end
+
+  test "Encrypt handles large shift number right" do
+    assert encrypt("abcd", 27) === "zabc"
+  end
+
+  test "Encrypt handles large shift number left" do
+    assert encrypt("abcd", -27) === "bcde"
+  end
+
 end
